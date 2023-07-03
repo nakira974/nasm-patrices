7e04fec4912a90b8e4c906400522a7162b7a36f6
nakira974 <maxime.loukhal@gmail.com>
2023-07-03 16:56:55 +0200

    Update message type to double word in sequence_generator.asm

--- sequence_generator.asm
+++ sequence_generator.asm
@@ -5,30 +5,29 @@
 
 
 section .data
-    format db "%s", 0
-    number db 10               ; Buffer size for the number
-    message db "Enter a number: ", 0
+    format db "%s", 10, 0
+    number db 32               ; Increase buffer size to 32 bytes
+    message db "Enter a number: ",10, 0
 
 section .text
     extern printf
     extern scanf
     extern atoi
+    global generate_sequence
 
-global _start
-
-_start:
+generate_sequence:
     sub rsp, 8                ; Align stack
 
     ; Print the message
-    mov rdi, format
-    mov rsi, message
-    xor rax, rax              ; Clear rax (used as placeholder for printf return value)
+    mov rdi, message
+    mov rsi, format            ; Swap the values of rdi and rsi
+    xor rax, rax               ; Clear rax (used as placeholder for printf return value)
     call printf
 
     ; Read input from the user
     lea rdi, [number]
     mov rsi, format
-    xor rax, rax              ; Clear rax (used as placeholder for scanf return value)
+    xor rax, rax               ; Clear rax (used as placeholder for scanf return value)
     call scanf
 
     ; Cast the input to an integer using atoi
@@ -37,6 +36,6 @@ _start:
 
     ; TODO: Store the resulting integer and perform further operations
 
-    add rsp, 8               ; Restore stack
-    xor eax, eax             ; Set exit code to 0
+    add rsp, 8                ; Restore stack
+    xor eax, eax              ; Set exit code to 0
     ret
