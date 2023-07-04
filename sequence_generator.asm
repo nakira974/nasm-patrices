25d3cb2955f48851fe7522732b8489cfbd10fcf1
nakira974 <maxime.loukhal@gmail.com>
2023-07-04 12:01:20 +0200

    Update loop

--- sequence_generator.asm
+++ sequence_generator.asm
@@ -20,57 +20,41 @@ section .text
     global generate_sequence
 
 generate_sequence:
-     and rsp, -16         ; Align stack to 16-byte boundary
-     sub rsp, 8           ; Align stack
+    push rbp             ; Preserve rbp on the stack
+    mov rbp, rsp         ; Set rbp as a base pointer for local variables
 
     ; Print the message
-    lea rcx, [message]         ; Pass the message as the first argument to printf
-    mov rdx, 0                 ; No floating-point values to print
+    lea rcx, [message]   ; Pass the message as the first argument to printf
+    xor rdx, rdx         ; No floating-point values to print
     call printf
 
     ; Read input from the user
-    add rsp, 8           ; Restore stack by freeing the space for the return address
-    pop rax              ; Restore the original value of rax
-    and rsp, -16         ; Align stack to 16-byte boundary
-    sub rsp, 8           ; Reserve space for the return address
-
-    lea rcx, [format]        ; Pass the format string as the first argument to scanf
-    lea rdx, [number]     ; Pass the buffer as the second argument to scanf
-    xor rax, rax              ; Clear rax (used as placeholder for scanf return value)
+    lea rcx, [format]    ; Pass the format string as the first argument to scanf
+    lea rdx, [number]    ; Pass the buffer as the second argument to scanf
+    xor rax, rax         ; Clear rax (used as placeholder for scanf return value)
     call scanf
 
+    ; Allocate memory for the sequence
+    mov eax, dword [number]   ; Get the input number
+    imul eax, 4               ; Calculate the total number of bytes needed (assuming int is 4 bytes)
+    push rax                  ; Save the calculated size on the stack
+    call malloc               ; Allocate memory for the sequence
+    add rsp, 8                ; Clean up the stack (remove the allocated size)
 
-    lea rcx, [format]
-    mov rdx, qword [number]
-    call printf
-    ; TODO: Store the resulting integer and perform further operations
-     add rsp, 8           ; Restore stack by freeing the space for the return address
-     pop rax              ; Restore the original value of rax
-     and rsp, -16         ; Align stack to 16-byte boundary
-     sub rsp, 8           ; Reserve space for the return address
-
-     ; Allocate memory for the sequence
-    mov rax, 32          ; Bytes per element (int is 4 bytes)
-    mul qword [number]   ; Calculate the total number of bytes needed
-    mov rdi, rax         ; Store the size in rdi
-    call malloc          ; Allocate memory for the sequence
-    mov qword [sequence], rax   ; Store the pointer to the allocated memory
+    ; Store the resulting integer and perform further operations
+    mov rdi, rax            ; Move the address of the allocated memory to rdi (for further operations)
 
     ; Generate the sequence in reverse order
-    ;mov rax, qword [number]
-    ;mov rcx, qword [sequence]
-    ;mov rbx, rax         ; Use rbx as a counter
-    ;xor edx, edx         ; Initialize index (edx) to 0
-    ;generate_loop:
-    ;      mov dword [rcx + rdx * 4], ebx   ; Store the current value in the sequence
-    ;      dec rbx              ; Decrease the counter
-    ;      inc edx              ; Increase the index
-    ;      cmp rbx, 0           ; Check if the counter has reached zero
-    ;      jg generate_loop     ; Jump back to the loop if it hasn't
+    mov ecx, dword [number] ; Use ecx as a counter
+    mov edx, 1              ; Initialize index (edx) to 1
+    generate_loop:
+        mov dword [rdi + rcx * 4 - 4], edx    ; Store the current value in the sequence
+        dec ecx                               ; Decrease the counter
+        inc edx                               ; Increase the index
+        cmp ecx, 0                            ; Check if the counter has reached zero
+        jg generate_loop                      ; Jump back to the loop if it hasn't
 
-   add rsp, 8
-   pop rax
-   and rsp, -16
-   sub rsp, 8
-   xor eax, eax             ; Set exit code to 0
-   ret
+    mov rsp, rbp               ; Restore the stack pointer
+    pop rbp                    ; Restore rbp
+    mov rax, qword [sequence]  ; Move the address of the generated sequence to rax
+    ret                        ; Return from the function
